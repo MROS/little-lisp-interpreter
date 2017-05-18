f3729ae690f26070adaf54b69f860fcc7f4420b7
MROS <yc1043@gmail.com>
2017-05-18 23:49:26 +0800

    將不同類型表達式分為不同class

--- interpreter.ts
+++ interpreter.ts
@@ -1,31 +1,3 @@
-enum ExpType {
-	LET,
-	Operator,
-	Variable,
-	Number,
-	Binding,
-	Let,
-}
-
-class Exp {
-	type: ExpType;
-	value: string | number;
-	constructor(type: ExpType, value: string | number) {
-		this.type = type;
-		this.value = value;
-	}
-}
-
-class ExpNode {
-	children: ExpNode[];
-	exp: Exp;
-	constructor(exp: Exp, children: ExpNode[]) {
-		this.exp = exp;
-		this.children = children;
-	}
-}
-
-// 試以正則表達式做更佳的分割
 function split_expression(program: string): string[] {
 	program = program.trim();
 	let buf = "";
@@ -65,88 +37,141 @@ function split_expression(program: string): string[] {
 	return exps;
 }
 
-function parser(program: string): ExpNode {
-	const exps = split_expression(program);
-	// console.log(`program: ${program}, exps: ${exps}`);
-	switch (true) {
-		// let
-		case /^let$/.test(exps[0]):
-			return new ExpNode(new Exp(ExpType.LET, "let"), [parser(exps[1]), parser(exps[2])]);
-		// 數字
-		case /^-?[0-9]+$/.test(exps[0]):
-			return new ExpNode(new Exp(ExpType.Number, Number(exps[0])), []);
-		// 加減乘除
-		case /^(\+|-|\*|\/)$/.test(exps[0]):
-			return new ExpNode(
-				new Exp(ExpType.Operator, exps[0]),
-				exps.slice(1).map((str) => parser(str)));
-		// 變數綁定
-		case /^\[[a-z-]+ (.+)\]$/.test(exps[0]):
-			let variable = /^\[([a-z-]+) (.+)\]$/.exec(exps[0])[1];
-			let target = /^\[([a-z-]+) (.+)\]$/.exec(exps[0])[2];
-			return new ExpNode(new Exp(ExpType.Binding, "binding"), [parser(variable), parser(target)]);
-		// 變數
-		case /^[a-z-]+/.test(exps[0]):
-			return new ExpNode(new Exp(ExpType.Variable, exps[0]), []);
+type ENV = Map<string, any>;
+
+interface EXP {
+	eval: (env: ENV) => any;
+}
+
+class LET {
+	variable: string;
+	target: EXP;
+	exp: EXP;
+	constructor(program: string) {
+		const exps = split_expression(program);
+		const regex = /^\(\[([a-z-]+) (.+)\]\)$/
+		if (regex.test(exps[1])) {
+			this.variable = regex.exec(exps[1])[1];
+			let target_str = regex.exec(exps[1])[2];
+			this.target = parser(target_str);
+		} else {
+			throw new Error(`${exps[1]} 並非一個綁定`);
+		}
+		this.exp = parser(exps[2]);
+	}
+	eval(env: ENV) {
+		env.set(this.variable, this.target.eval(env));
+		let ret = this.exp.eval(env);
+		env.delete(this.variable);
+		return ret;
 	}
 }
 
-function show(node, level) {
-	console.log(`${' '.repeat(level * 4)}${node.exp.value}`);
-	for (let c of node.children) {
-		show(c, level + 1);
+class LAMBDA {
+	variable: string;
+	exp: EXP;
+	constructor(program: string) {
+		const exps = split_expression(program);
+		if (/\(([a-z]+)\)/.test(exps[1])) {
+			this.variable = /\(([a-z]+)\)/.exec(exps[1])[1];
+		} else {
+			throw new Error("lambda 後沒接變數");
+		}
+		this.exp = parser(exps[2]);
+	}
+	eval(env: ENV) {
+		
 	}
 }
 
-// TODO: 需要處理同變數重複綁定
-function eval_with_env(node: ExpNode, env: Map<string, ExpNode>) {
-	switch (node.exp.type) {
-		case ExpType.Number: {
-			return node.exp.value;
-		}
-		case ExpType.Variable: {
-			// 若node.exp.type 為 ExpType.Variable ，則 node.exp.value 在建構時必為字串，然而 TypeScript 的型態系統無法推導
-			// 故強制轉型
-			let variable = String(node.exp.value);
-			if (env.has(variable)) {
-				return eval_with_env(env.get(variable), env);
+class Operation {
+	operator: string;
+	arg1: EXP;
+	arg2: EXP;
+	constructor(program: string) {
+		const exps = split_expression(program);
+		this.operator = exps[0];
+		this.arg1 = parser(exps[1]);
+		this.arg2 = parser(exps[2]);
+	}
+	eval(env: ENV) {
+		switch (this.operator) {
+			case "+": {
+				return this.arg1.eval(env) + this.arg2.eval(env);
 			}
-			throw new Error("變數 ${variable} 未綁定到任何值");
-		}
-		case ExpType.LET: {
-			let binding = node.children[0], exp = node.children[1];
-			let variable = String(binding.children[0].exp.value);
-			env.set(variable, binding.children[1]);
-			let ret = eval_with_env(exp, env);
-			env.delete(variable);
-			return ret;
-		}
-		case ExpType.Operator: {
-			switch (node.exp.value) {
-				case "+": {
-					return eval_with_env(node.children[0], env) + eval_with_env(node.children[1], env);
-				}
-				case "-": {
-					return eval_with_env(node.children[0], env) - eval_with_env(node.children[1], env);
-				}
-				case "*": {
-					return eval_with_env(node.children[0], env) * eval_with_env(node.children[1], env);
-				}
-				case "/": {
-					return eval_with_env(node.children[0], env) / eval_with_env(node.children[1], env);
-				}
+			case "-": {
+				return this.arg1.eval(env) - this.arg2.eval(env);
 			}
+			case "*": {
+				return this.arg1.eval(env) * this.arg2.eval(env);
+			}
+			case "/": {
+				return this.arg1.eval(env) / this.arg2.eval(env);
+			}
+		}
+	}
+}
+
+class Variable {
+	name: string;
+	constructor(program: string) {
+		this.name = program;
+	}
+	eval(env: ENV) {
+		if (env.has(this.name)) {
+			let exp = env.get(this.name);
+			return exp;
+		} else {
+			throw new Error(`變數 ${this.name} 未綁定到任何值`);
+		}
+	}
+}
+
+class LispNumber {
+	n: number;
+	constructor(program: string) {
+		this.n = Number(program);
+	}
+	eval(env: ENV) {
+		return this.n;
+	}
+}
+
+function parser(program: string): EXP {
+	if (/\(.+\)/.test(program)) {
+		const exps = split_expression(program);
+		switch (true) {
+			// lambda
+			case /^lambda$/.test(exps[0]):
+				return new LAMBDA(program);
+			// let
+			case /^let$/.test(exps[0]):
+				return new LET(program);
+			// 加減乘除
+			case /^(\+|-|\*|\/)$/.test(exps[0]):
+				return new Operation(program);
+		}
+	} else { // 外面沒有括號的表達式，只該是變數或數字
+		switch (true) {
+			// 變數
+			case /^[a-z_]+/.test(program):
+				return new Variable(program);
+			// 數字
+			case /^-?[0-9]+$/.test(program):
+				return new LispNumber(program);
 		}
 	}
 }
 
 export function interpreter(program: string) {
 	let root = parser(program);
-	// show(root, 0);
-	let value = eval_with_env(root, new Map<string, ExpNode>());
+	let env = new Map<string, EXP>();
+	// console.log(root);
+	let value = root.eval(env);
 	// console.log(`${program} 的求值結果為 ${value}`);
 	return value
 }
 
-interpreter("(let ([a (let ([b 1]) b)]) (+ 1 a))");
-// interpreter("(let ([a 3]) (let ([yuja a]) (- yuja (+ a 2))))");
+// interpreter("(let ([a (let ([b 1]) b)]) (+ 1 a))");
+// interpreter("(let ([a 3]) a)");
+// interpreter("(let ([a 3]) (let ([yuja a]) (- 	yuja (+ a 2))))");
